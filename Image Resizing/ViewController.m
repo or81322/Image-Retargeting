450b4bdc85aded9a793c79a20028c3f49b4e6954
Or Maayan <ormaayan@michael-leybovichs-Mac-mini.local>
2014-08-05 14:27:12 +0300

    added tap gesture recognizer that show/hide status & navigationTab bars

--- Image Resizing/ViewController.m
+++ Image Resizing/ViewController.m
@@ -26,4 +26,14 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)startAutomaticRetargeting:(id)sender {
+    [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationSlide];
+    [[self navigationController] setNavigationBarHidden:YES animated:YES];
+}
+
+- (IBAction)userTouchedTheScreen:(id)sender {
+    [[UIApplication sharedApplication] setStatusBarHidden:![[UIApplication sharedApplication] isStatusBarHidden] withAnimation:UIStatusBarAnimationSlide];
+    [self.navigationController setNavigationBarHidden:!self.navigationController.navigationBar.isHidden animated:YES];
+}
+
 @end
