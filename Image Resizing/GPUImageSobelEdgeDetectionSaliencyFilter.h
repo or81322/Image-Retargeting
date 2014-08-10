70f43b08ae99301b1bdddec0ccb11496366a8fed
Or Maayan <ormaayan@michael-leybovichs-Mac-mini.local>
2014-08-11 02:07:45 +0300

    added implementation of the SaliencyFilter using GPUImage library.

---
+++ Image Resizing/GPUImageSobelEdgeDetectionSaliencyFilter.h
@@ -0,0 +1,13 @@
+//
+//  GPUImageSobelEdgeDetectionSaliencyFilter.h
+//  Image Resizing
+//
+//  Created by Or Maayan on 8/11/14.
+//  Copyright (c) 2014 Or Maayan & Micheal Leybovich. All rights reserved.
+//
+
+#import "SaliencyFilter.h"
+
+@interface GPUImageSobelEdgeDetectionSaliencyFilter : SaliencyFilter
+
+@end
---
+++ Image Resizing/GPUImageSobelEdgeDetectionSaliencyFilter.m
@@ -0,0 +1,28 @@
+//
+//  GPUImageSobelEdgeDetectionSaliencyFilter.m
+//  Image Resizing
+//
+//  Created by Or Maayan on 8/11/14.
+//  Copyright (c) 2014 Or Maayan & Micheal Leybovich. All rights reserved.
+//
+
+#import "GPUImageSobelEdgeDetectionSaliencyFilter.h"
+
+@implementation GPUImageSobelEdgeDetectionSaliencyFilter
+
+-(UIImage *)getSaliencyImage:(UIImage *)image {
+    GPUImagePicture *stillImageSource = [[GPUImagePicture alloc] initWithImage:image];
+    GPUImageGrayscaleFilter *grayscaleFilter = [[GPUImageGrayscaleFilter alloc] init];
+    GPUImageSobelEdgeDetectionFilter *sobelEdgeDetectionFilter = [[GPUImageSobelEdgeDetectionFilter alloc] init];
+    
+    [stillImageSource addTarget:grayscaleFilter];
+    [stillImageSource addTarget:sobelEdgeDetectionFilter];
+    
+    [sobelEdgeDetectionFilter useNextFrameForImageCapture];
+    [stillImageSource processImage];
+    
+    return [stillImageFilter imageFromCurrentFramebuffer];
+}
+
+
+@end
