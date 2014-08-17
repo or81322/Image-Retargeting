96ee7d12a10f95a3952807c70e36f54028513f3a
or maayan <ormaayan@tomzlotkmansmac.cs.technion.ac.il>
2014-08-17 14:47:26 +0300

    added UIImage Scale category with scale method (used to scale down the saliency map).

---
+++ Image Resizing/UIImage+Scale.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+Scale.h
+//  Image Resizing
+//
+//  Created by or maayan on 8/17/14.
+//  Copyright (c) 2014 Or Maayan & Micheal Leybovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Scale)
+
+-(UIImage*)scaleToSize:(CGSize)size;
+
+@end
